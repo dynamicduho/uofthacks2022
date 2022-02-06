0153cf31519d4fe184c5a05a53d8ee1796b1ae17
Tom Shu <87506153+TheUltimateDab@users.noreply.github.com>
2022-02-06 05:47:50 -0500

    changed forward-to-police.js

--- twilio/forward-to-police.js
+++ twilio/forward-to-police.js
@@ -24,11 +24,11 @@ const getMessage = async (phone_number) => {
 exports.handler = async function (context, event, callback) {
   // The pre-initialized Twilio Client is available from the `context` object
   const twilioClient = context.getTwilioClient();
-  const userInfo = await getMessage("");
+  const userInfo = await getMessage(event.from); // event.from is user phone number
 
   // Query parameters or values sent in a POST body can be accessed from `event`
-  const from = event.From || '+12262127233'; // the Safeline hotline number
-  const to = event.To || '+YourNumberHere'; // number removed for privacy
+  const from = /*event.From ||*/ '+12262127233'; // the Safeline hotline number
+  const to = /*event.To ||*/ 'Enter Police number or any other number';
   const body = event.Body || `${userInfo}`;
 
   // Use `messages.create` to generate a message. Be sure to chain with `then`
@@ -47,28 +47,4 @@ exports.handler = async function (context, event, callback) {
       console.error(error);
       return callback(error);
     });
-  
-  // Note that TwiML can be hosted at a URL and accessed by Twilio
-  const url = event.Url || ''; // add your own URL here
-
-  // Use `calls.create` to place a phone call. Be sure to chain with `then`
-  // and `catch` to properly handle the promise and call `callback` _after_ the
-  // call is placed successfully!
-  twilioClient.calls
-    .create({ to, from, url })
-    .then((call) => {
-      console.log('Call successfully placed');
-      console.log(call.sid);
-      // Make sure to only call `callback` once everything is finished, and to pass
-      // null as the first parameter to signal successful execution.
-      return callback(null, `Success! Call SID: ${call.sid}`);
-    })
-    .catch((error) => {
-      console.error(error);
-      return callback(error);
-    });
 };
-
-
-
-
